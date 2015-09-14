435cdcf1c7abb2d110e4aad1478f488cdfe42560
achinthau <achintha@duosoftware.com>
2015-09-14 15:23:58 +0530

    upload config files

---
+++ config/custom-environment-variables.js
@@ -0,0 +1,21 @@
+/**
+ * Created by dinusha on 4/22/2015.
+ */
+
+module.exports = {
+
+    "DB": {
+        "Type":"SYS_DATABASE_TYPE",
+        "User":"SYS_DATABASE_POSTGRES_USER",
+        "Password":"SYS_DATABASE_POSTGRES_PASSWORD",
+        "Port":"SYS_SQL_PORT",
+        "Host":"SYS_DATABASE_HOST",
+        "Database":"SYS_DATABASE_POSTGRES_USER"
+    },
+
+    "Host":{
+        "Ip":"HOST_NAME",
+        "Port":"HOST_CLUSTER_PORT",
+        "Version":"HOST_VERSION"
+    }
+};
---
+++ config/default.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "DB": {
+    "Type":"mysql",
+    "User":"root",
+    "Password":"DuoS123",
+    "Port":3306,
+    "Host":"20.20.20.5",
+    "Database":"radius"
+  },
+  "Host":{
+    "Ip":"localhost",
+    "Port":"9881",
+    "Version":"1.0"
+  }
+};
